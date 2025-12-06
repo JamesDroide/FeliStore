711503f9af0c52e1102908751e3ceb2717757591
JamesDroide <james200166@gmail.com>
2025-12-06 16:35:43 -0500

    Feat: Implementacion de FeliMarket

--- BackEnd/src/config/contracts.config.js
+++ BackEnd/src/config/contracts.config.js
@@ -1,14 +1,14 @@
 // Configuración automática de contratos desplegados
-// Generado el: 2025-12-06T12:25:30.871Z
+// Generado el: 2025-12-06T21:33:52.039Z
 // Red: localhost
 
 export const contractAddresses = {
-  "FelicoinToken": "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-  "IdentityRegistry": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-  "LoyaltyPayment": "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
-  "EventTicket": "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
-  "FelicoinStaking": "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
-  "FelicoinGovernor": "0x0165878A594ca255338adfa4d48449f69242Eb8F"
+  "FelicoinToken": "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6",
+  "IdentityRegistry": "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
+  "LoyaltyPayment": "0x610178dA211FEF7D417bC0e6FeD39F05609AD788",
+  "EventTicket": "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0",
+  "FelicoinStaking": "0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82",
+  "FelicoinGovernor": "0x9A676e781A523b5d0C0e43731313A708CB607508"
 };
 
 export const networkConfig = {
